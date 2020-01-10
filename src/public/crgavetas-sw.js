9b2e72613baefe8a223154634677a1e48f7924ec
Carlos <socarlosb@gmail.com>
2020-01-10 13:23:06 +0000

    v1.0.4 sw cache first

--- src/public/crgavetas-sw.js
+++ src/public/crgavetas-sw.js
@@ -1,50 +1,66 @@
 // This is the "Offline page" service worker
 
-const CACHE = "crgavetas-v1.0.3";
+const CACHE = "crgavetas-v1.0.4";
 
 const offlineFallbackPage = "offline.html";
+const precacheFiles = [
+  /* Add an array of files to precache for your app */
+];
 
 // Install stage sets up the offline page in the cache and opens a new cache
 self.addEventListener("install", function(event) {
   console.log("Install Event processing");
 
+  console.log("Skip waiting on install");
+  self.skipWaiting();
+
   event.waitUntil(
     caches.open(CACHE).then(function(cache) {
-      console.log("Cached offline page during install");
-
-      if (offlineFallbackPage === "offline.html") {
-        return cache.add(
-          new Response(
-            "TODO: Update the value of the offlineFallbackPage constant in the serviceworker."
-          )
-        );
-      }
-
-      return cache.add(offlineFallbackPage);
+      console.log("Caching pages during install");
+      return cache.addAll(precacheFiles);
     })
   );
 });
 
+/ Allow sw to control of current page
+self.addEventListener("activate", function (event) {
+  console.log("Claiming clients for current page");
+  event.waitUntil(self.clients.claim());
+});
+
 // If any fetch fails, it will look for the request in the cache and serve it from there first
 self.addEventListener("fetch", function(event) {
-  if (event.request.method !== "GET") return;
+if (event.request.method !== "GET") return;
 
   event.respondWith(
-    fetch(event.request)
-      .then(function(response) {
-        console.log("Add page to offline cache: " + response.url);
+    fromCache(event.request).then(
+      function (response) {
+        // The response was found in the cache so we responde with it and update the entry
 
-        // If request was success, add or update it in the cache
-        event.waitUntil(updateCache(event.request, response.clone()));
+        // This is where we call the server to get the newest version of the
+        // file to use the next time we show view
+        event.waitUntil(
+          fetch(event.request).then(function (response) {
+            return updateCache(event.request, response);
+          })
+        );
 
         return response;
-      })
-      .catch(function(error) {
-        console.log(
-          "Network request Failed. Serving content from cache: " + error
-        );
-        return fromCache(event.request);
-      })
+      },
+      function () {
+        // The response was not found in the cache so we look for it on the server
+        return fetch(event.request)
+          .then(function (response) {
+            // If request was success, add or update it in the cache
+            event.waitUntil(updateCache(event.request, response.clone()));
+
+            return response;
+          })
+          .catch(function (error) {
+            console.log("[PWA Builder] Network request failed and no cache." + error);
+          });
+      }
+    )
   );
 });
 
