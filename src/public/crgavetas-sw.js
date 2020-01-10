16c13ac5de3feae1f71c6de452c1d8b5ed55cf8d
Carlos <socarlosb@gmail.com>
2020-01-10 13:06:13 +0000

    full cache

--- src/public/crgavetas-sw.js
+++ src/public/crgavetas-sw.js
@@ -1,59 +1,86 @@
 // This is the "Offline page" service worker
 
-const CACHE = "crgavetas-v1.0.1";
+const CACHE = "crgavetas-v1.0.2";
 
-const offlineFallbackPage = "offline.html";
+const precacheFiles = [
+  /* Add an array of files to precache for your app */
+];
 
-// Install stage sets up the offline page in the cache and opens a new cache
 self.addEventListener("install", function(event) {
-  console.log("Install Event processing");
+  console.log("[PWA Builder] Install Event processing");
+
+  console.log("[PWA Builder] Skip waiting on install");
+  self.skipWaiting();
 
   event.waitUntil(
     caches.open(CACHE).then(function(cache) {
-      console.log("Cached offline page during install");
-      return cache.add(offlineFallbackPage);
+      console.log("[PWA Builder] Caching pages during install");
+      return cache.addAll(precacheFiles);
     })
   );
 });
 
 // Allow sw to control of current page
 self.addEventListener("activate", function(event) {
-  console.log("Claiming clients for current page");
+  console.log("[PWA Builder] Claiming clients for current page");
   event.waitUntil(self.clients.claim());
 });
 
-// If any fetch fails, it will show the offline page.
+// If any fetch fails, it will look for the request in the cache and serve it from there first
 self.addEventListener("fetch", function(event) {
   if (event.request.method !== "GET") return;
 
   event.respondWith(
-    fetch(event.request).catch(function(error) {
-      // The following validates that the request was for a navigation to a new document
-      if (
-        event.request.destination !== "document" ||
-        event.request.mode !== "navigate"
-      ) {
-        return;
-      }
+    fromCache(event.request).then(
+      function(response) {
+        // The response was found in the cache so we responde with it and update the entry
 
-      console.error("Network request Failed. Serving offline page " + error);
-      return caches.open(CACHE).then(function(cache) {
-        return cache.match(offlineFallbackPage);
-      });
-    })
+        // This is where we call the server to get the newest version of the
+        // file to use the next time we show view
+        event.waitUntil(
+          fetch(event.request).then(function(response) {
+            return updateCache(event.request, response);
+          })
+        );
+
+        return response;
+      },
+      function() {
+        // The response was not found in the cache so we look for it on the server
+        return fetch(event.request)
+          .then(function(response) {
+            // If request was success, add or update it in the cache
+            event.waitUntil(updateCache(event.request, response.clone()));
+
+            return response;
+          })
+          .catch(function(error) {
+            console.log(
+              "[PWA Builder] Network request failed and no cache." + error
+            );
+          });
+      }
+    )
   );
 });
 
-// This is an event that can be fired from your page to tell the SW to update the offline page
-self.addEventListener("refreshOffline", function() {
-  const offlinePageRequest = new Request(offlineFallbackPage);
+function fromCache(request) {
+  // Check to see if you have it in the cache
+  // Return response
+  // If not in the cache, then return
+  return caches.open(CACHE).then(function(cache) {
+    return cache.match(request).then(function(matching) {
+      if (!matching || matching.status === 404) {
+        return Promise.reject("no-match");
+      }
 
-  return fetch(offlineFallbackPage).then(function(response) {
-    return caches.open(CACHE).then(function(cache) {
-      console.log(
-        "Offline page updated from refreshOffline event: " + response.url
-      );
-      return cache.put(offlinePageRequest, response);
+      return matching;
     });
   });
-});
+}
+
+function updateCache(request, response) {
+  return caches.open(CACHE).then(function(cache) {
+    return cache.put(request, response);
+  });
+}
