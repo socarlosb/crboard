ab3cb192de100975d5b9f755e5d35932a6f94db7
Carlos <socarlosb@gmail.com>
2020-01-24 15:42:09 +0000

    clean up code

--- src/app.js
+++ src/app.js
@@ -1,6 +1,4 @@
 const express = require("express");
-const path = require("path");
-const { getClan } = require("./helper/serverApi");
 
 const app = express();
 
@@ -44,14 +42,5 @@ app.get("/", (req, res) => {
     </html>
   `);
 });
-
-// app.get("/req", (req, res) => {
-//   res.sendFile(path.join(__dirname, "/public/req.html"));
-// });
-// app.get("/clan/:clanTag", async (req, res) => {
-//   const isClan = await getClan(req.params.clanTag);
-//   if (isClan) return res.sendFile(path.join(__dirname, "/public/clan.html"));
-//   return res.sendFile(path.join(__dirname, "/public/404.html"));
-// });
 
 module.exports = app;
