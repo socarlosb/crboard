064d449c08b194dcc46a1e81f611ccc5d2b83b5a
Carlos <socarlosb@gmail.com>
2020-04-19 11:57:07 +0100

    add new route for any clan

--- src/controllers/clans.js
+++ src/controllers/clans.js
@@ -1,8 +1,10 @@
 const {
   updateClanRequirements,
   getClan,
-  getClans
+  getClans,
 } = require("../helper/serverApi");
+
+const { getClanInfo2 } = require("../helper/vendorAPI");
 
 // @desc Update clan requirements
 // @route POST /api/v1/clan/:clanTag
@@ -15,7 +17,7 @@ exports.updateClanRequirements = async (req, res, next) => {
     if (updated.message) throw new Error(updated.message);
 
     return res.status(200).json({
-      success: true
+      success: true,
     });
   } catch (err) {
     console.error({ error: err.message });
@@ -39,7 +41,7 @@ exports.getClanInfo = async (req, res, next) => {
     return res.status(200).json({
       success: true,
       count: clan.length,
-      data: clan
+      data: clan,
     });
   } catch (err) {
     console.error({ error: err.message });
@@ -59,7 +61,30 @@ exports.getAllClans = async (req, res, next) => {
     return res.status(200).json({
       success: true,
       count: clans.length,
-      data: clans
+      data: clans,
+    });
+  } catch (err) {
+    console.error({ error: err.message });
+    res
+      .status(404)
+      .json({ success: false, error: err.message || "Server error" });
+  }
+};
+
+// @desc  Get all clan data
+// @route GET /api/v1/clan/out/:clanTag
+// @access Public
+exports.getOutClanInfo = async (req, res, next) => {
+  try {
+    const { id } = req.params;
+
+    const clan = await getClanInfo2(id);
+
+    if (!clan) throw new Error("Clan not found");
+
+    return res.status(200).json({
+      success: true,
+      data: clan,
     });
   } catch (err) {
     console.error({ error: err.message });
--- src/routes/clan.js
+++ src/routes/clan.js
@@ -3,12 +3,14 @@ const {
   updateClanRequirements,
   getClanInfo,
   getAllClans,
+  getOutClanInfo,
 } = require("../controllers/clans");
 const { isAdmin } = require("../helper/apiAuth");
 
 const router = express.Router();
 
 router.route("/:id").get(getClanInfo).post(isAdmin, updateClanRequirements);
+router.route("/out/:id").get(getOutClanInfo);
 
 router.route("/").get(getAllClans);
 
