e276d8b0862e3889b4a87f647aabfee61c2a3021
Carlos <socarlosb@gmail.com>
2020-05-06 22:59:18 +0100

    add option to search other clans

--- src/controllers/clans.js
+++ src/controllers/clans.js
@@ -3,8 +3,18 @@ const {
   getClan,
   getClans,
 } = require("../helper/serverApi");
+const {
+  asyncForEach,
+  delay,
+  getCardPercentage2,
+  parseDate,
+} = require("../helper/funcs");
 
-const { getClanInfo2 } = require("../helper/vendorAPI");
+const {
+  getClanInfo2,
+  getClanWarLogs2,
+  getPlayerStats2,
+} = require("../helper/vendorAPI");
 
 // @desc Update clan requirements
 // @route POST /api/v1/clan/:clanTag
@@ -76,12 +86,81 @@ exports.getAllClans = async (req, res, next) => {
 // @access Public
 exports.getOutClanInfo = async (req, res, next) => {
   try {
-    const { id } = req.params;
+    const { id: clanTag } = req.params;
 
-    const clan = await getClanInfo2(id);
+    // const clan = await getClanInfo2(id);
 
-    if (!clan) throw new Error("Clan not found");
+    // if (!clan) throw new Error("Clan not found");
 
+    // return res.status(200).json({
+    //   success: true,
+    //   data: clan,
+    // });
+
+    await delay(`Getting Clan Info for ${clanTag}`, 1000);
+
+    const {
+      memberList,
+      name,
+      description,
+      clanScore,
+      clanWarTrophies,
+      members,
+      requiredTrophies,
+    } = await getClanInfo2(clanTag);
+    if (!memberList) throw new Error(`Royale API connection failed!`);
+    const clanWarLogs = await getClanWarLogs2(clanTag);
+    await delay(`Getting Clan War Logs: ${name}`, 1000);
+
+    const inClanMembers = [];
+    await asyncForEach(memberList, async (member) => {
+      await delay(
+        `Getting player info stats ${member.name} (${member.tag})`,
+        1000
+      );
+      const memberStats = await getPlayerInfo(
+        member.tag.split("#")[1],
+        clanWarLogs
+      );
+      const {
+        tag,
+        clanRank,
+        previousClanRank,
+        name,
+        role,
+        trophies,
+        donations,
+      } = member;
+
+      inClanMembers.push({
+        tag,
+        rank: clanRank,
+        previousClanRank,
+        name,
+        role,
+        trophies,
+        donations,
+        ...memberStats,
+      });
+    });
+
+    await delay(`Updating internal clan ${name} (${clanTag})`, 1000);
+
+    let lastWarDate = "";
+    if (clanWarLogs[0] && clanWarLogs[0].createdDate)
+      lastWarDate = parseDate(clanWarLogs[0].createdDate);
+
+    const clan = {
+      tag: clanTag.toLowerCase(),
+      name,
+      description,
+      lastWarDate,
+      score: clanScore,
+      warTrophies: clanWarTrophies,
+      memberCount: members,
+      requiredScore: requiredTrophies,
+      members: inClanMembers,
+    };
     return res.status(200).json({
       success: true,
       data: clan,
@@ -92,4 +171,69 @@ exports.getOutClanInfo = async (req, res, next) => {
       .status(404)
       .json({ success: false, error: err.message || "Server error" });
   }
+};
+
+const getPlayerInfo = async (playerTag, clanWarLogs) => {
+  const playerStats = await getPlayerStats2(playerTag);
+
+  const warLogs = await getClanWarWinRate(clanWarLogs, playerTag);
+
+  let winRate = 0;
+
+  if (warLogs.battleCount > 0)
+    winRate = warLogs.wins / (warLogs.battleCount + warLogs.battlesExtra);
+
+  const cardsFound = playerStats.cards.length;
+
+  return {
+    stats: {
+      warWinRate: winRate.toFixed(2),
+      warDayWins: playerStats.warDayWins,
+      allWinRate: (
+        (playerStats.wins / (playerStats.wins + playerStats.losses)) *
+        100
+      ).toFixed(0),
+      level: playerStats.expLevel,
+      cardLevels: {
+        max: getCardPercentage2(playerStats.cards, 13, cardsFound),
+        legend: getCardPercentage2(playerStats.cards, 12, cardsFound),
+        gold: getCardPercentage2(playerStats.cards, 11, cardsFound),
+        silver: getCardPercentage2(playerStats.cards, 10, cardsFound),
+        bronze: getCardPercentage2(playerStats.cards, 9, cardsFound),
+      },
+    },
+    warStats: warLogs,
+  };
+};
+const getClanWarWinRate = async (clanWarLogs, playerTag) => {
+  const warStats = {
+    cardsEarned: 0,
+    battleCount: 0,
+    battlesPlayed: 0,
+    battlesMissed: 0,
+    battlesExtra: 0,
+    wins: 0,
+    collectionDayBattlesPlayed: 0,
+  };
+
+  clanWarLogs.map((war) => {
+    war.participants.map((player) => {
+      const battlesMissed = player.numberOfBattles - player.battlesPlayed;
+      if (player.tag === `#${playerTag}`) {
+        warStats.cardsEarned += player.cardsEarned;
+        if (player.numberOfBattles > 1) {
+          warStats.battleCount += 1;
+          warStats.battlesExtra += 1;
+        } else {
+          warStats.battleCount += player.numberOfBattles;
+        }
+        warStats.battlesPlayed += player.battlesPlayed;
+        warStats.battlesMissed += battlesMissed;
+        warStats.wins += player.wins;
+        warStats.collectionDayBattlesPlayed +=
+          player.collectionDayBattlesPlayed;
+      }
+    });
+  });
+  return warStats;
 };
---
+++ src/public/js/other.js
@@ -0,0 +1,439 @@
+const membersBody = document.querySelector("#members");
+const clanName = document.querySelector("#clanName");
+const clanDesc = document.querySelector("#clanDesc");
+const clanUpdated = document.querySelector("#clanUpdated");
+const th = document.getElementsByTagName("th");
+const average = document.querySelector("#average");
+const topDonators = document.querySelector("#topDonators");
+const lastDonators = document.querySelector("#lastDonators");
+const topWarWinRate = document.querySelector("#topWarWinRate");
+const lastWarWinRate = document.querySelector("#lastWarWinRate");
+const clanTagInput = document.querySelector("#clanTag");
+const getClan = document.querySelector("#getClan");
+const errorNotification = document.querySelector("#error");
+
+new Tablesort(document.querySelector("table"), {
+  descending: true,
+});
+
+Tablesort.extend(
+  "role",
+  function (item) {
+    return item.search(/(leader|coLeader|elder|member)/i) !== -1;
+  },
+  function (a, b) {
+    var monthNames = ["leader", "coLeader", "elder", "member"];
+    return monthNames.indexOf(b) - monthNames.indexOf(a);
+  }
+);
+
+function compareValues(key, order = "asc") {
+  return function innerSort(a, b) {
+    if (!a.hasOwnProperty(key) || !b.hasOwnProperty(key)) {
+      console.log("property doesn't exist on either object");
+      return 0;
+    }
+
+    const varA = typeof a[key] === "string" ? a[key].toUpperCase() : a[key];
+    const varB = typeof b[key] === "string" ? b[key].toUpperCase() : b[key];
+    let comparison = 0;
+    if (varA > varB) {
+      comparison = 1;
+    } else if (varA < varB) {
+      comparison = -1;
+    }
+    return order === "desc" ? comparison * -1 : comparison;
+  };
+}
+
+function compareSubValues(prop, arr, order = "asc") {
+  prop = prop.split(".");
+  var len = prop.length;
+
+  // return order === "desc" ? comparison * -1 : comparison;
+
+  arr.sort(function (a, b) {
+    var i = 0;
+    while (i < len) {
+      a = a[prop[i]];
+      b = b[prop[i]];
+      i++;
+    }
+    if (a < b) {
+      return order === "desc" ? -1 : 1;
+    } else if (a > b) {
+      return order === "desc" ? 1 : -1;
+    } else {
+      return 0;
+    }
+  });
+  return arr;
+}
+
+async function getClanInfo() {
+  const clanTag = clanTagInput.value;
+  try {
+    // return fetch("https://crboard.herokuapp.com/api/v1/clan/out/" + clanTag.replace("#", ""))
+    return fetch(
+      "http://localhost:4444/api/v1/clan/out/" + clanTag.replace("#", "")
+    )
+      .then((resp) => resp.json())
+      .then((json) => {
+        return json.data;
+      });
+  } catch (error) {
+    console.log("ops", error.message);
+    errorNotification.innerHTML = error.message;
+  }
+}
+
+getClan.addEventListener("click", async () => {
+  if (!clanTag.value) {
+    errorNotification.innerHTML = "Please add clan tag!";
+    return;
+  }
+  try {
+    getClan.classList.add("is-loading");
+    getClan.disabled = true;
+
+    const totals = {
+      level: 0,
+      trophies: 0,
+      allWinRate: 0,
+      warDayWins: 0,
+      donations: 0,
+      battleCount: 0,
+      warMia: 0,
+      warWinRate: 0,
+      warCardsEarned: 0,
+      warAvgCollections: 0,
+      cardMax: 0,
+      cardLegend: 0,
+      cardGold: 0,
+      cardSilver: 0,
+      cardBronze: 0,
+    };
+    let activeMembers = 0;
+
+    membersBody.innerHTML = "Loading...";
+    const clan = await getClanInfo();
+    console.info("clan", clan);
+    console.info("----------------");
+    membersBody.innerHTML = "";
+
+    document.title = `${clan.name} (#${clan.tag})`;
+    clanName.innerHTML = `${clan.name} (#${clan.tag})
+    <a target="_blank" href="https://royaleapi.com/clan/${clan.tag}">
+      <img class="imgSmall" src="https://royaleapi.com/static/img/branding/cr-api-logo.png" alt="RoyaleAPI.com"/>
+    </a>`;
+    clanDesc.innerHTML = `${clan.description}`;
+    clanUpdated.innerHTML = `Updated ${moment(clan.updatedAt).fromNow()} with ${
+      clan.members.length
+    } members ${
+      clan.lastWarDate
+        ? `(Last war data was ${moment(clan.lastWarDate).fromNow()})`
+        : ""
+    }`;
+
+    clan.members.map((member) => {
+      member.warStats.battleCount > 0 ? (activeMembers += 1) : null;
+
+      totals.level += member.stats.level || 0;
+      totals.trophies += member.trophies || 0;
+      totals.allWinRate += parseInt(member.stats.allWinRate) || 0;
+      totals.warDayWins += member.stats.warDayWins || 0;
+      totals.donations += member.donations || 0;
+      totals.battleCount += member.warStats.battleCount || 0;
+      totals.warMia += member.warStats.battlesMissed || 0;
+      totals.warWinRate +=
+        Number((member.stats.warWinRate * 100).toFixed(0)) || 0;
+      totals.warCardsEarned +=
+        Number(
+          (member.warStats.cardsEarned / member.warStats.battleCount).toFixed(0)
+        ) || 0;
+      totals.warAvgCollections +=
+        Number(
+          (
+            member.warStats.collectionDayBattlesPlayed /
+            member.warStats.battleCount
+          ).toFixed(2)
+        ) || 0;
+      totals.cardMax +=
+        Number((member.stats.cardLevels.max * 100).toFixed(0)) || 0;
+      totals.cardLegend +=
+        Number((member.stats.cardLevels.legend * 100).toFixed(0)) || 0;
+      totals.cardGold +=
+        Number((member.stats.cardLevels.gold * 100).toFixed(0)) || 0;
+      totals.cardSilver +=
+        Number((member.stats.cardLevels.silver * 100).toFixed(0)) || 0;
+      totals.cardBronze +=
+        Number((member.stats.cardLevels.bronze * 100).toFixed(0)) || 0;
+
+      let classRole = "",
+        requiredTrophies = "",
+        warDayWins = "",
+        classWarMia = "",
+        warWinRate = "",
+        warAvgCollections = "",
+        cardLevelsMax = "",
+        cardLevelsLegend = "",
+        cardLevelsGold = "",
+        cardLevelsSilver = "",
+        cardLevelsBronze = "",
+        level = "",
+        allWinRate = "";
+
+      if (member.role === "leader") classRole = "has-text-danger";
+      if (member.role === "coLeader") classRole = "has-text-info";
+      if (member.role === "elder") classRole = "has-text-success";
+
+      membersBody.innerHTML += `
+    <tr >
+      <td class='has-text-centered'>${member.rank} ${
+        member.rank < member.previousClanRank
+          ? '<img style="width:0.8em" src="/img/green.png"></img>'
+          : member.rank > member.previousClanRank
+          ? '<img style="width:0.8em" src="/img/red.png"></img>'
+          : ""
+      }</td>
+      <td>${
+        member.name
+      } <a target="_blank" href="https://royaleapi.com/player/${
+        member.tag.split("#")[1]
+      }">
+        <img class="imgSmall" src="https://royaleapi.com/static/img/branding/cr-api-logo.png" alt="RoyaleAPI.com"/>
+      </a></td>
+      <td class=${classRole} >${member.role}</td>
+      <td class=${level} >${member.stats.level}</td>
+      <td class="table-trophies has-text-centered ${requiredTrophies}">
+        <img src="${getPlayerTrophiesBadge(member.trophies)}"></img>
+      </td>
+      <td class="has-text-centered ${requiredTrophies}">
+        ${member.trophies}
+      </td>
+      <td class='has-text-centered ${allWinRate}'>${
+        member.stats.allWinRate
+      }</td>
+      <td class='has-text-centered ${warDayWins}'>${
+        member.stats.warDayWins
+      }</td>
+      <td class='has-text-centered'>${member.donations}</td>
+      <td class='has-text-centered'>${member.warStats.battleCount}</td>
+      <td class='has-text-centered'>${member.warStats.battlesMissed}</td>
+      <td class='has-text-centered ${warWinRate}'>
+      ${(member.stats.warWinRate * 100).toFixed(0)}%
+      </td>
+      <td class='has-text-centered'}>
+      ${
+        member.warStats.cardsEarned
+          ? (member.warStats.cardsEarned / member.warStats.battleCount).toFixed(
+              0
+            )
+          : "---"
+      }
+      </td>
+      <td class='has-text-centered ${warAvgCollections}'>
+      ${
+        member.warStats.collectionDayBattlesPlayed
+          ? (
+              member.warStats.collectionDayBattlesPlayed /
+              member.warStats.battleCount
+            ).toFixed(2)
+          : "---"
+      }
+      </td>
+      
+      <td class='has-text-centered ${cardLevelsMax}'>
+      ${(member.stats.cardLevels.max * 100).toFixed(0)}% 
+      </td>
+      <td class='has-text-centered ${cardLevelsLegend}'>
+      ${(member.stats.cardLevels.legend * 100).toFixed(0)}%
+      </td>
+      <td class='has-text-centered ${cardLevelsGold}'>
+      ${(member.stats.cardLevels.gold * 100).toFixed(0)}% 
+      </td>
+      <td class='has-text-centered ${cardLevelsSilver}'>
+      ${(member.stats.cardLevels.silver * 100).toFixed(0)}% 
+      </td>
+      <td class='has-text-centered ${cardLevelsBronze}'>
+        ${(member.stats.cardLevels.bronze * 100).toFixed(0)}%
+      </td>
+    </tr>
+  `;
+    });
+
+    // Top 5 Stuff
+    const membersOrderByName = clan.members.sort(compareValues("name"));
+
+    // Top 5 War Win Rate
+    const byWarWinRateTotal = compareSubValues(
+      "stats.warWinRate",
+      membersOrderByName
+    );
+
+    const byWarWinRate = byWarWinRateTotal.filter(
+      (member) => member.warStats.battleCount > 0
+    );
+
+    byWarWinRate.slice(0, 5).map((member, index) => {
+      topWarWinRate.innerHTML += `
+      <li>${index + 1} - ${member.name} (${(
+        member.stats.warWinRate * 100
+      ).toFixed(0)}% in ${member.warStats.battlesPlayed} battle wars)</li>
+        `;
+    });
+
+    // Last 5 War Win Rate
+    byWarWinRate
+      .slice(byWarWinRate.length - 5, byWarWinRate.length)
+      .map((member, index) => {
+        lastWarWinRate.innerHTML += `
+      <li>${byWarWinRate.length - 4 + index} - ${member.name} (${(
+          member.stats.warWinRate * 100
+        ).toFixed(0)}% in ${member.warStats.battlesPlayed} battle wars)</li>
+    `;
+      });
+
+    // Top 5 Donations
+    const byDonations = membersOrderByName.sort(
+      compareValues("donations", "desc")
+    );
+    byDonations.slice(0, 5).map((member, index) => {
+      topDonators.innerHTML += `
+    <li>${index + 1} - ${member.name} (${member.donations} cards)</li>
+    `;
+    });
+
+    // Last 5 Donations
+    byDonations
+      .slice(byDonations.length - 5, byDonations.length)
+      .map((member, index) => {
+        lastDonators.innerHTML += `
+    <li>${byDonations.length - 4 + index} - ${member.name} (${
+          member.donations
+        } cards)</li>
+    `;
+      });
+
+    average.innerHTML = `
+    <th class="has-text-centered"> - </th>  
+    <th class="has-text-centered">Global Average</th>
+    <th class="has-text-centered"> --- </th>
+    <th class="has-text-centered">${(totals.level / clan.memberCount).toFixed(
+      0
+    )}</th>
+    <th class="has-text-centered"> --- </th>
+    <th class="numeric-sort has-text-centered">${(
+      totals.trophies / clan.memberCount
+    ).toFixed(0)}</th>
+    <th class="numeric-sort has-text-centered">${(
+      totals.allWinRate / clan.memberCount
+    ).toFixed(0)}</th>
+    <th class="numeric-sort has-text-centered">${(
+      totals.warDayWins / clan.memberCount
+    ).toFixed(0)}</th> 
+    <th class="numeric-sort has-text-centered">${(
+      totals.donations / clan.memberCount
+    ).toFixed(0)}</th>
+    <th class="numeric-sort has-text-centered">${(
+      totals.battleCount / clan.memberCount
+    ).toFixed(0)}</th>
+    <th class="numeric-sort has-text-centered">${(
+      totals.warMia / clan.memberCount
+    ).toFixed(2)}</th>
+    <th class="numeric-sort has-text-centered">${(
+      totals.warWinRate / activeMembers
+    ).toFixed(0)}%</th>
+    <th class="numeric-sort has-text-centered">${(
+      totals.warCardsEarned / clan.memberCount
+    ).toFixed(0)}</th>
+    <th class="numeric-sort has-text-centered">${(
+      totals.warAvgCollections / clan.memberCount
+    ).toFixed(2)}</th>
+    <th class="has-text-centered"><abbr title="Cards Max">${(
+      totals.cardMax / clan.memberCount
+    ).toFixed(0)}%</th>
+    <th class="has-text-centered"><abbr title="Cards Legend">${(
+      totals.cardLegend / clan.memberCount
+    ).toFixed(0)}%</th>
+    <th class="has-text-centered"><abbr title="Cards Gold">${(
+      totals.cardGold / clan.memberCount
+    ).toFixed(0)}%</th>
+    <th class="has-text-centered"><abbr title="Cards Silver">${(
+      totals.cardSilver / clan.memberCount
+    ).toFixed(0)}%</th>
+    <th class="has-text-centered"><abbr title="Cards Bronze">${(
+      totals.cardBronze / clan.memberCount
+    ).toFixed(0)}%</th>
+  `;
+    getClan.classList.remove("is-loading");
+    getClan.disabled = false;
+  } catch (error) {
+    console.error(error);
+
+    errorNotification.innerHTML = error;
+  }
+});
+
+function getPlayerTrophiesBadge(trophies) {
+  const playerTrophiesBadge = {
+    ultimateChampion: "https://royaleapi.com/static/img/arenas/arena22.png",
+    royalChampion: "https://royaleapi.com/static/img/arenas/arena21.png",
+    grandChampion: "https://royaleapi.com/static/img/arenas/arena20.png",
+    champion: "https://royaleapi.com/static/img/arenas/arena19.png",
+    master3: "https://royaleapi.com/static/img/arenas/arena18.png",
+    master2: "https://royaleapi.com/static/img/arenas/arena17.png",
+    master1: "https://royaleapi.com/static/img/arenas/arena16.png",
+    challenger3: "https://royaleapi.com/static/img/arenas/arena15.png",
+    challenger2: "https://royaleapi.com/static/img/arenas/arena14.png",
+    legendary: "https://royaleapi.com/static/img/arenas/arena13.png",
+  };
+
+  let badge = "";
+
+  switch (true) {
+    case trophies >= 7000:
+      badge = playerTrophiesBadge.ultimateChampion;
+      break;
+    case trophies >= 6600:
+      badge = playerTrophiesBadge.royalChampion;
+      break;
+    case trophies >= 6300:
+      badge = playerTrophiesBadge.grandChampion;
+      break;
+    case trophies >= 6000:
+      badge = playerTrophiesBadge.champion;
+      break;
+    case trophies >= 5600:
+      badge = playerTrophiesBadge.master3;
+      break;
+    case trophies >= 5300:
+      badge = playerTrophiesBadge.master2;
+      break;
+    case trophies >= 5000:
+      badge = playerTrophiesBadge.master1;
+      break;
+    case trophies >= 4600:
+      badge = playerTrophiesBadge.challenger3;
+      break;
+    case trophies >= 4300:
+      badge = playerTrophiesBadge.challenger2;
+      break;
+    case trophies >= 4000:
+      badge = playerTrophiesBadge.legendary;
+      break;
+    default:
+      badge = "";
+      break;
+  }
+  return badge;
+}
+
+function copyToClipboard(text) {
+  var dummy = document.createElement("textarea");
+  document.body.appendChild(dummy);
+  dummy.value = text;
+  dummy.select();
+  document.execCommand("copy");
+  document.body.removeChild(dummy);
+}
